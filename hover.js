e47768523454e5fb7afc2314411f64b7fadf8138
Kiriil-Shark05 <k.gagarin22@gmail.com>
2021-07-13 10:20:48 +0300

    header and three blocck

---
+++ hover.js
@@ -0,0 +1,36 @@
+  let a1 = document.querySelector('.header-login-item2');
+  let a2 = document.querySelector('.section-main-text-button1');
+  let a3 = document.querySelector('.section-pay-text-button1');
+  let a4 = document.querySelector('.section-pay2-text-button1');
+
+  a1.onmouseover = function a1_true() {
+    a1.classList.add('active')
+  }
+
+  a1.onmouseout = function a1_false() {
+    a1.classList.remove('active')
+  }
+
+  a2.onmouseover = function a2_true() {
+    a2.classList.add('active')
+  }
+
+  a2.onmouseout = function a2_false() {
+    a2.classList.remove('active')
+  }
+
+  a3.onmouseover = function a3_true() {
+    a3.classList.add('active')
+  }
+
+  a3.onmouseout = function a3_false() {
+    a3.classList.remove('active')
+  }
+
+  a4.onmouseover = function a4_true() {
+    a4.classList.add('active')
+  }
+
+  a4.onmouseout = function a4_false() {
+    a4.classList.remove('active')
+  }
